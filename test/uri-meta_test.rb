1f64b8c78e1c469f7d44e9b5826b37beb203677b
Danial Pearce <git@tigris.id.au>
2009-08-13 21:29:22 +1000

    Basic tests

--- test/uri-meta_test.rb
+++ test/uri-meta_test.rb
@@ -1,7 +1,89 @@
 require 'test_helper'
 
 class UriMetaTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  context 'URI.parse(http://staging.metauri.com/)' do
+    context '.meta' do
+      setup do
+        @uri = URI.parse('http://staging.metauri.com/')
+      end
+
+      should 'exist' do
+        @uri.respond_to? :meta
+      end
+
+      should 'be a URI::Meta object' do
+        assert_kind_of URI::Meta, @uri.meta
+      end
+
+      context '.uri' do
+        should 'be a URI object' do
+          assert_kind_of URI, @uri.meta.uri
+        end
+
+        should 'not have been a redirect' do
+          assert_equal @uri.to_s, @uri.meta.uri.to_s
+        end
+      end
+
+      context '.title' do
+        should 'be Meta URI' do
+          assert_equal 'Meta URI', @uri.meta.title
+        end
+      end
+
+      context '.headers' do
+        should 'be nil' do
+          assert_nil @uri.meta.headers
+        end
+      end
+
+      context '.content' do
+        should 'be nil' do
+          assert_nil @uri.meta.content
+        end
+      end
+    end
+
+    context '.meta(:content => true)' do
+      context '.content' do
+        should 'be populated'
+      end
+
+      context '.headers' do
+        should 'be nil'
+      end
+    end
+
+    context '.meta(:headers => true)' do
+      context '.headers' do
+        should 'be populated'
+      end
+
+      context '.content' do
+        should 'be nil'
+      end
+    end
+  end
+
+  context 'URI.parse(http://staging.metauri.com/redirect)' do
+    setup do
+      @uri = URI.parse('http://staging.metauri.com/redirect')
+    end
+
+    context '.meta' do
+      context '.uri' do
+        should 'be a redirect' do
+          assert_not_equal @uri.to_s, @uri.meta.uri.to_s
+        end
+      end
+    end
+  end
+
+  context 'URI.parse(garbage).meta' do
+    should 'raise errors' do
+      assert_raise ArgumentError do
+        URI.parse('garbage').meta
+      end
+    end
   end
 end
